4a9a9f4cabe425dbb53f69b624a68c31f0ac9966
sl0bby <danik.zaycev2020@gmail.com>
2025-03-10 18:58:56 +0300

    Lesson 47 revision

--- task03/logic.cpp
+++ task03/logic.cpp
@@ -22,29 +22,8 @@ int findMaxIndex(int* array, int size) {
 	}
 	return maxIndex;
 }
-int findMinIndex(int* array, int size) {
-	int minIndex = array[0];
 
-	for (int i = 0; i < size; i++)
-	{
-		if (array[i] < minIndex) {
-			minIndex = array[i];
-		}
-	}
-	return minIndex;
-}
 
-int findMaxIndex(int* array, int size) {
-	int maxIndex = 0;
-
-	for (int i = 0; i < size; i++)
-	{
-		if (array[i] > maxIndex) {
-			maxIndex = array[i];
-		}
-	}
-	return maxIndex;
-}
 
 
 double get_avg_of_non_extreme_elems(int* array, int size) {
